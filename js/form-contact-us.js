40f82446aed99f9c3ae1c377389531d9dfd31d07
Rian Prasetyo <rianpra201@gmail.com>
2023-12-11 15:26:07 +0700

    modified:   account.html 	modified:   js/form-contact-us.js modified:   signup.html

---
+++ js/form-contact-us.js
@@ -0,0 +1,32 @@
+// form-contact-us.js
+
+function signIn() {
+  const username = document.getElementById("usernameSignIn").value;
+  const password = document.getElementById("passwordSignIn").value;
+
+  fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data.message);
+    });
+}
+
+function signUp() {
+  const username = document.getElementById("usernameSignUp").value;
+  const email = document.getElementById("emailSignUp").value;
+  const password = document.getElementById("passwordSignUp").value;
+
+  fetch("http://localhost:3000/signup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, email, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data.message);
+    });
+}
