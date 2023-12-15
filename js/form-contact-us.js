2c9dff64521f265cded62a92291aceadc189cb9f
Dewi Sinta Febriani <136657807+Dewisinta12@users.noreply.github.com>
2023-12-15 16:25:45 +0700

    Update form-contact-us.js

--- js/form-contact-us.js
+++ js/form-contact-us.js
@@ -17,9 +17,9 @@ function contact() {
 }
 
 function signUp() {
-  const username = document.getElementById("usernameSignUp").value;
-  const email = document.getElementById("emailSignUp").value;
-  const password = document.getElementById("passwordSignUp").value;
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
   fetch("http://localhost:3000/register", {
     method: "POST",
