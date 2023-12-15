9939ba30c8da64fa8f18dff6fcce035a092c10a6
Rian Prasetyo <rianpra201@gmail.com>
2023-12-15 16:09:04 +0700

    modified:   homepage.html modified:   js/script.js

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// script.js
+
+function handleResponsive() {
+  const exploreLink = document.getElementById("explore-link");
+  const scrollProduct = document.querySelector(".scroll-product");
+  if (window.innerWidth <= 600) {
+    scrollProduct.textContent = "EXPLORE";
+  } else {
+    scrollProduct.textContent = "EXPLORE OUR PRODUCTS";
+  }
+}
+window.addEventListener("resize", handleResponsive);
+handleResponsive();
+
+//     Script Homepage
+// JS SCRIPT.JS
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Add a click event listener to the "Learn more" buttons
+  var learnMoreButtons = document.querySelectorAll(
+    ".learn-more, .learn-more2, .learn-more3, .learn-more4"
+  );
+  learnMoreButtons.forEach(function (button) {
+    button.addEventListener("click", function (event) {
+      // Handle the click event, you can add your logic here
+      alert("Learn more clicked!");
+    });
+  });
+
+  // Toggle class based on window size
+  window.addEventListener("resize", function () {
+    toggleClassBasedOnWindowSize();
+  });
+
+  toggleClassBasedOnWindowSize();
+
+  function toggleClassBasedOnWindowSize() {
+    var screenWidth = window.innerWidth;
+
+    if (screenWidth <= 600) {
+      document.body.classList.add("small-screen");
+    } else {
+      document.body.classList.remove("small-screen");
+    }
+  }
+
+  var menuToggle = document.querySelector(".menu-toggle");
+  var navBar = document.querySelector(".nav-bar");
+
+  if (menuToggle && navBar) {
+    menuToggle.addEventListener("click", function () {
+      navBar.classList.toggle("show-menu");
+    });
+  }
+
+  var sliderImages = document.querySelectorAll(".slider img");
+  var currentSlide = 0;
+  function showSlide(index) {
+    sliderImages.forEach(function (image) {
+      image.style.display = "none";
+    });
+    sliderImages[index].style.display = "block";
+  }
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % sliderImages.length;
+    showSlide(currentSlide);
+  }
+  setInterval(nextSlide, 5000);
+});
